0d2d4e6acfa7311b50d164aa49d53cb0b571b0cc
Mayank Sankhla <94232011+Mayanksankhla2003@users.noreply.github.com>
2024-03-20 22:32:44 +0530

    Add files via upload

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+    return (
+        <>
+            <Card username="John" btnText="Visit Again!!" />
+            <br />
+            <Card />
+        </>
+    );
+}
+
+export default App;
