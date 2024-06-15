5ca26c3e551638fb82a926f33930c3251a8f8eaa
Mayank Sankhla <94232011+Mayanksankhla2003@users.noreply.github.com>
2024-06-16 00:19:19 +0530

    Redux Toolkit

---
+++ reduxToolkitTodo/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "./Components/AddTodo";
+import Todos from "./Components/Todos";
+
+function App() {
+    return (
+        <>
+            <h1>Learn about Redux Toolkit</h1>
+            <AddTodo />
+            <Todos />
+        </>
+    );
+}
+
+export default App;
